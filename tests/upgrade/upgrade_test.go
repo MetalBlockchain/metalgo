5adb36484ef83896de6e14196d281d6df758dd4d
Glenn <705252+MlennGarien@users.noreply.github.com>
2023-08-25 15:05:32 +0200

    Fix deps

--- tests/upgrade/upgrade_test.go
+++ tests/upgrade/upgrade_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 
-	runner_sdk "github.com/ava-labs/avalanche-network-runner-sdk"
+	runner_sdk "github.com/MetalBlockchain/metal-network-runner-sdk"
 
 	"github.com/MetalBlockchain/metalgo/tests"
 )
