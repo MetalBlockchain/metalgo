dc2e0512e86f8667f7a9f9b8514ff999988e3428
Glenn <705252+MlennGarien@users.noreply.github.com>
2024-03-01 15:20:14 +0100

    Update constants.go

--- version/constants.go
+++ version/constants.go
@@ -100,18 +100,18 @@ var (
 	}
 
 	ApricotPhasePre6Times = map[uint32]time.Time{
-		constants.MainnetID: time.Date(2022, time.September, 5, 1, 30, 0, 0, time.UTC),
-		constants.TahoeID:   time.Date(2022, time.September, 6, 20, 0, 0, 0, time.UTC),
-	}
-
-	ApricotPhase6Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2022, time.September, 8, 20, 0, 0, 0, time.UTC),
 		constants.TahoeID:   time.Date(2022, time.September, 8, 20, 0, 0, 0, time.UTC),
 	}
 
+	ApricotPhase6Times = map[uint32]time.Time{
+		constants.MainnetID: time.Date(2022, time.September, 8, 22, 0, 0, 0, time.UTC),
+		constants.TahoeID:   time.Date(2022, time.September, 8, 22, 0, 0, 0, time.UTC),
+	}
+
 	ApricotPhasePost6Times = map[uint32]time.Time{
 		constants.MainnetID: time.Date(2022, time.September, 9, 3, 0, 0, 0, time.UTC),
-		constants.TahoeID:   time.Date(2022, time.September, 9, 6, 0, 0, 0, time.UTC),
+		constants.TahoeID:   time.Date(2022, time.September, 9, 3, 0, 0, 0, time.UTC),
 	}
 
 	BanffTimes = map[uint32]time.Time{
