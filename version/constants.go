9091def12ec14f89ecdbbd7b8d1853887d9e5dc6
Glenn <705252+MlennGarien@users.noreply.github.com>
2024-03-06 19:26:17 +0100

    Update Durango timestamp

--- version/constants.go
+++ version/constants.go
@@ -138,7 +138,7 @@ var (
 
 	DurangoTimes = map[uint32]time.Time{
 		constants.MainnetID: time.Date(10000, time.March, 1, 0, 0, 0, 0, time.UTC),
-		constants.TahoeID:   time.Date(10000, time.March, 11, 0, 0, 0, 0, time.UTC),
+		constants.TahoeID:   time.Date(2024, time.March, 18, 0, 0, 0, 0, time.UTC),
 	}
 )
 
